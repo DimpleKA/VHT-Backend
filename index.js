757b96e23a07b61cb6b6d46f4d43735fa7d26e72
DimpleKA <153168007+DimpleKA@users.noreply.github.com>
2024-12-06 08:57:50 +0530

    lkj

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use("/api/users/", userRoutes);
 
 // Connect to the database (Uncomment to use)
-const URI = process.env.MONGO_URI || "mongodb+srv://vatsal00:Dimple9@cluster0.cvuh3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const URI = process.env.MONGO_URI || "mongodb+srv://vatsal00:Dimple9@cluster0.cvuh3.mongodb.net/Vht?retryWrites=true&w=majority&appName=Cluster0";
 connectDB(URI);
 
 // Start the server
